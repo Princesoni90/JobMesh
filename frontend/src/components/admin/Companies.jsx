d8e3a1d27759f992fda2fd2699b668feec161c19
Princesoni90 <Princesoni3684@gmail.com>
2024-12-07 11:34:27 +0530

    responsive

--- frontend/src/components/admin/Companies.jsx
+++ frontend/src/components/admin/Companies.jsx
@@ -20,8 +20,8 @@ const Companies = () => {
     return (
         <div>
             <Navbar />
-            <div className='max-w-6xl mx-auto my-10'>
-                <div className='flex items-center justify-between my-5'>
+            <div className='max-w-6xl mx-auto my-10 p-2'>
+                <div className='flex items-center justify-between my-5 p-2'>
                     <Input
                         className="w-fit"
                         placeholder="Filter by name"
